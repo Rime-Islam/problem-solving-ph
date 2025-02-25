3571ed02fbd67659d72b8778c57d2a9139886e33
rime-islam <rimeislam672@gmail.com>
2025-02-25 12:12:23 +0600

    prob-9

--- SecretNinja01.js
+++ SecretNinja01.js
@@ -61,3 +61,19 @@ function romanToInt(s) {
     return total;
 };
 console.log(romanToInt('III'));
+
+// PW2.01.09 longest-common-prefix
+function longestCommonPrefix(strs) {
+    if (!strs.length) return "";
+
+    let prefix = strs[0];
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1);
+            if (!prefix) return "";
+        }
+    }
+    return prefix;
+};
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+console.log(longestCommonPrefix(["dog", "flow", "car"]));
